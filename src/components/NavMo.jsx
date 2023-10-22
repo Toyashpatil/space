f6edfd51d8a5c65647225209c324ffae5493c4eb
Toyash <toyashpatil17@gmail.com>
2023-10-23 00:23:16 +0530

    feat: mobile-menu

--- src/components/NavMo.jsx
+++ src/components/NavMo.jsx
@@ -1,25 +1,67 @@
+import AuthContext from '@/context/AuthContext'
 import Image from 'next/image'
-import React from 'react'
+import Link from 'next/link'
+import React, { useContext } from 'react'
 
 const NavMo = () => {
+    const { open, setOpen } = useContext(AuthContext)
     return (
         <div className='flex justify-center'>
             <div className='w-[100%] h-[96px] flex items-center gap-4 justify-start bg-blue '>
-            <div className='ml-10 flex'>
+                <div className='ml-10 flex'>
 
-            <Image
-                src="/Group.png"
-                width={50}
-                height={50}
-            />
+                    <Image
+                        src="/Group.png"
+                        width={50}
+                        height={50}
+                    />
+                </div>
+                <div className=' w-[100%] gap-10 justify-end flex mr-10 '>
+                    <button onClick={() => {
+                        setOpen((prev) => !prev)
+                    }
+                    }>
+                        <Image
+                            src="/Group2.png"
+                            width={20}
+                            height={20}
+                        />
+                    </button>
+                </div>
+
             </div>
-            <div className=' w-[100%] gap-10 justify-end flex mr-10 '>
-            <Image
-                src="/Group2.png"
-                width={20}
-                height={20}
-            />
-            </div>
+            <div className='absolute'>
+                {open && <div className="flex  items-center  justify-center">
+                    <div className=" scale-up-tr  absolute rounded-lg Glass_morph flex items-center mt-[320px] ml-[200px] justify-center w-[200px]  h-[200px] ">
+                        <div className='flex-col space-y-5'>
+                            <div>
+                                <Link href='/' onClick={() => {
+                                    setOpen((prev) => !prev)
+                                }
+                                }  ><h1 className=" text_two ">00 Hello</h1></Link>
+                            </div>
+                            <div>
+                                <Link href='/destination' onClick={() => {
+                                    setOpen((prev) => !prev)
+                                }
+                                } ><h1 className=" text_two ">01 Destination</h1></Link>
+                            </div>
+                            <div>
+                                <Link href='/crew' onClick={() => {
+                                    setOpen((prev) => !prev)
+                                }
+                                } ><h1 className=" text_two ">02 Crew</h1></Link>
+                            </div>
+                            <div>
+                                <Link href='/tech' onClick={() => {
+                                    setOpen((prev) => !prev)
+                                }
+                                }><h1 className=" text_two ">03 Technology</h1></Link>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>}
 
             </div>
         </div>
--- src/context/Authstate.js
+++ src/context/Authstate.js
@@ -5,9 +5,10 @@ import AuthContext from './AuthContext'
 const Authstate = (props) => {
 
     const [planetIndex, setPlanetIndex] = useState(0)
+    const [open, setOpen] = useState(false)
 
     return (
-        <AuthContext.Provider value={{ planetIndex, setPlanetIndex }}>
+        <AuthContext.Provider value={{ planetIndex, setPlanetIndex ,open,setOpen}}>
             {props.children}
         </AuthContext.Provider>
     )
